fd8f3c1834fb14bdb413984cc851bd9b366710c1
Ankit sharma <unknownsdeveloper@gmail.com>
2017-04-24 11:24:04 +0530

    Initial commit

---
+++ app/src/main/java/ankit/developer/unknowns/expandablerecyclerview/Adapter/MyAdapter.java
@@ -0,0 +1,59 @@
+package ankit.developer.unknowns.expandablerecyclerview.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bignerdranch.expandablerecyclerview.Adapter.ExpandableRecyclerAdapter;
+import com.bignerdranch.expandablerecyclerview.Model.ParentObject;
+
+import java.util.List;
+
+import ankit.developer.unknowns.expandablerecyclerview.Models.TitleChild;
+import ankit.developer.unknowns.expandablerecyclerview.Models.TitleParent;
+import ankit.developer.unknowns.expandablerecyclerview.R;
+import ankit.developer.unknowns.expandablerecyclerview.ViewHolders.TitleChildViewHolder;
+import ankit.developer.unknowns.expandablerecyclerview.ViewHolders.TitleParentViewHolder;
+
+
+/**
+ * Created by reale on 23/11/2016.
+ */
+
+public class MyAdapter extends ExpandableRecyclerAdapter<TitleParentViewHolder,TitleChildViewHolder> {
+
+    LayoutInflater inflater;
+
+    public MyAdapter(Context context, List<ParentObject> parentItemList) {
+        super(context, parentItemList);
+        inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public TitleParentViewHolder onCreateParentViewHolder(ViewGroup viewGroup) {
+       View view = inflater.inflate(R.layout.list_parent,viewGroup,false);
+        return new TitleParentViewHolder(view);
+    }
+
+    @Override
+    public TitleChildViewHolder onCreateChildViewHolder(ViewGroup viewGroup) {
+        View view = inflater.inflate(R.layout.list_child,viewGroup,false);
+        return new TitleChildViewHolder(view);
+    }
+
+    @Override
+    public void onBindParentViewHolder(TitleParentViewHolder titleParentViewHolder, int i, Object o) {
+        TitleParent title = (TitleParent)o;
+        titleParentViewHolder._textView.setText(title.getTitle());
+
+    }
+
+    @Override
+    public void onBindChildViewHolder(TitleChildViewHolder titleChildViewHolder, int i, Object o) {
+        TitleChild title = (TitleChild)o;
+        titleChildViewHolder.option1.setText(title.getOption1());
+        titleChildViewHolder.option2.setText(title.getOption2());
+
+    }
+}
---
+++ app/src/main/java/ankit/developer/unknowns/expandablerecyclerview/MainActivity.java
@@ -0,0 +1,59 @@
+package ankit.developer.unknowns.expandablerecyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.bignerdranch.expandablerecyclerview.Model.ParentObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ankit.developer.unknowns.expandablerecyclerview.Adapter.MyAdapter;
+import ankit.developer.unknowns.expandablerecyclerview.Models.TitleChild;
+import ankit.developer.unknowns.expandablerecyclerview.Models.TitleCreator;
+import ankit.developer.unknowns.expandablerecyclerview.Models.TitleParent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        ((MyAdapter)recyclerView.getAdapter()).onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView)findViewById(R.id.myRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        MyAdapter adapter = new MyAdapter(this,initData());
+        adapter.setParentClickableViewAnimationDefaultDuration();
+        adapter.setParentAndIconExpandOnClick(true);
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    private List<ParentObject> initData() {
+        TitleCreator titleCreator = TitleCreator.get(this);
+        List<TitleParent> titles = titleCreator.getAll();
+        List<ParentObject> parentObject = new ArrayList<>();
+        for(TitleParent title:titles)
+        {
+            List<Object> childList = new ArrayList<>();
+            childList.add(new TitleChild("Add to contacts","Send message"));
+            title.setChildObjectList(childList);
+            parentObject.add(title);
+        }
+        return parentObject;
+
+    }
+}
